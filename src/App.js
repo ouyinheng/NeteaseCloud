57fd57cde23ee13cb31e78923b81a865ed8b4c56
ouyinheng <2675379992@qq.com>
2018-07-31 15:23:55 +0800

    'scss'

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {BrowserRouter as Router,Route,Link} from 'react-router-dom'
+import Home from './pages/home/Home.js'
+import Recomm from './pages/recomm/Recomm.js'
+import Video from './pages/video/Video.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <Router>
+          <div className="App">
+            <header className="nav">
+                <div className="left">
+                  <span className="iconfont icon-view-list-o"></span>
+                </div>
+                <div className="center">
+                  <Link to="/">
+                    <span className="iconfont icon-yinle"></span>
+                  </Link>
+                  <Link to="/recomm">
+                    <span className="iconfont icon-wangyiyunyinlezizhi-copy"></span>
+                  </Link>
+                  <Link to="/video">
+                    <span className="iconfont icon-shipin"></span>
+                  </Link>
+                </div>
+                <div className="right">
+                  <span className="iconfont icon-sousuo"></span>
+                </div>
+            </header>
+            <section className="container">
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/recomm" component={Recomm}/>
+              <Route exact path="/video" component={Video}/>
+            </section>
+            <footer className="control"></footer>
+          </div>  
+        </Router>
     );
   }
 }
---
+++ src/pages/recomm/Recomm.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class Recomm extends Component {
+    render() {
+        return (
+            <div>Recomm</div>
+        )
+    }
+}
+export default Recomm
