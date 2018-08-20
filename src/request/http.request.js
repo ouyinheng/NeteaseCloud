f8d3033f412ebfccf510d61eb762041abdb4bc99
ouyinheng <2675379992@qq.com>
2018-08-20 23:21:03 +0800

    'cahnge'

--- src/request/http.request.js
+++ src/request/http.request.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const base = 'http://192.168.1.236:3001'
+// const base = 'http://192.168.1.236:3001'
+const base = 'http://192.168.70.1:3001'
 
 export const toSearch = params => {return axios.get(`${base}/search?keywords=${params}`).then(res =>res.data);}
 export const getHot = params => {return axios.get(`${base}/search/hot`).then(res =>res.data);}
